928704f4c6b23392467a42fcb25d618b300feb23
Marek Ch <mchqwerty@gmail.com>
2013-09-19 15:26:53 +0200

    core: add generic macros
    add macros: assertf (assert with formated output) and dbg (debug message)

---
+++ src/wit-assert.h
@@ -0,0 +1,23 @@
+#ifndef __WIT_ASSERT_H__
+#define __WIT_ASSERT_H__
+
+/* Assert with formated output */
+#define assertf(cond, ...) 							\
+	do {									\
+		if (!(cond)) {							\
+			fprintf(stderr, "%s (%s: %d): Assertion %s failed!",	\
+					__FUNCTION__, __FILE__, __LINE__, #cond);\
+			fprintf(stderr, " " __VA_ARGS__);			\
+			putc('\n', stderr);					\
+			abort();						\
+		}								\
+	} while (0)
+
+#define dbg(...) 								\
+	do {									\
+		fprintf(stderr, "{{%6d | %s in %s: %d: | ", getpid(),		\
+				__FUNCTION__, __FILE__, __LINE__);		\
+		fprintf(stderr,	__VA_ARGS__);					\
+	} while (0)
+
+#endif /* __WIT_ASSERT_H__ */
