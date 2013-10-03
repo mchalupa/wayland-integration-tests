e1ca30fe000758cb4d19f92eeffb31e29d888f9e
Marek Ch <mchqwerty@gmail.com>
2013-10-03 14:53:08 +0200

    global: add comments to enum
    Add description of arguments

--- src/wit-global.h
+++ src/wit-global.h
@@ -7,10 +7,19 @@
 
 
 enum optype {
+	/* arguments: int can (0 or 1)*/
 	CAN_CONTINUE = 1,	/* client can continue */
+
+	/* arguments: int count*/
 	EVENT_COUNT,	   	/* how many events can display emit */
+
+	/* arguments: struct wit_event, union wl_argument *args */
 	EVENT_EMIT,		/* ask for single event */
+
+	/* arguments: none */
 	RUN_FUNC,		/* run user's func */
+
+	/* arguments: size_t size, void *mem */
 	SEND_BYTES		/* send raw bytestream to the other side */
 };
 
